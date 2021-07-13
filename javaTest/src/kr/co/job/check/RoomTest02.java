067faf9cae5b38b3c0144a484f175bb6642c57c1
YONSAI <YONSAI@402-28>
2021-07-13 15:11:54 +0900

    "Reserve System"

--- javaTest/src/kr/co/job/check/RoomTest02.java
+++ javaTest/src/kr/co/job/check/RoomTest02.java
@@ -1,5 +1,6 @@
 /*
- * + 숙제 예약 시, 예약자명 이 들어가도록
+ * 예약 시, 예약자명, 예약번호, 예약인원 입력받기
+ * 예약취소시 예약번호를 기준으로 취소한다
  */
 
 package kr.co.job.check;
@@ -11,37 +12,37 @@ public class RoomTest02 {
 	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int[] room = new int[12];
-		int num = 0;
+		
+		int[] room = new int[12];				// 방 개수
+		int[] hostNum = new int[12];			// 인원
+		int[] hostCode = new int[12];			// 예약번호
+		String[] hostName = new String[12];		// 예약자명
+		
+		int num = 0;	// 버튼선택 토글
 
 		boolean done = true;
 		
 		while (done) {
-			System.out.println("==============================================");
-			System.out.println("1.예약하기  2.예약취소  3.방 확인  4.개인정보입력  99.종료");
-			System.out.println("==============================================");
+			System.out.println("==================================");
+			System.out.println("1.예약하기  2.예약취소  3.방 확인  99.종료");
+			System.out.println("==================================");
 			
 			System.out.print("선택하기 : ");
 			num = scan.nextInt();
 			
 			switch (num) {
 				case 1:
-					RoomList(room);
-					RoomReserve(room);
-					break;	// 함수 바깥으로 나감.
+					roomList(room, hostName, hostNum, hostCode);
+					roomReserve(room, hostName, hostNum, hostCode);
+					break;
 					
 				case 2:
-					RoomList(room);
-					RoomCancel(room);
+					System.out.println("취소를 진행합니다.");		
+					roomCancel(room, hostName, hostNum, hostCode);
 					break;
 					
 				case 3:
-					RoomList(room);
-					break;
-				
-				case 4:
-					System.out.println("개인정보입력 페이지로 이동합니다.");
-					createId();
+					roomList(room, hostName, hostNum, hostCode);
 					break;
 					
 				case 99:
@@ -63,42 +64,51 @@ public class RoomTest02 {
 		
 	}
 
-	private static void createId() {
-		System.out.println("");
-		System.out.println("----------------개인정보입력------------");
-		System.out.println("1.이름  2.휴대폰번호  3.주소  4.성별  5.나이");
-		System.out.println("------------------------------------");
-		
-		
-	}
 
-	private static void RoomCancel(int[] room) {	// 예약취소 매서드
+
+	private static void roomCancel(int[] room, String[] hostName, int[] hostNum, int[] hostCode) {	// ●●●●● 예약취소 매서드
 		
 		boolean result = true;	// 이 boolean변수는 예약취소 매서드가 main밖에 위치해있기 때문에 함수 내에서 초기화를 해주어야함
 		
 		// 배열초과시 에러메세지 탈출코드
 		while (result) {	// 반복문 while을 입력해서 조건문 불충족시 바로 아래 번호입력란으로 계속 돌아감
-			System.out.print("예약취소 번호입력 : ");
-			int num = scan.nextInt();
+			System.out.print("예약번호 입력 : ");
+			int num = scan.nextInt();	// 설정된 예약번호(4자리)를 입력 (뒷자리는 배열 순서와 동일한 숫자를 가짐)
+			num = num - 1000;			// 데이터를 배열번호에 옮겨야하기때문에 네자리(+1000)을 다시 배열순서번호로(-1000을 해서) 돌려놓음
 			
-			if (num > 12) {		// 예약취소번호가 배열번호를 초과했을 시
+			if (num > room.length || num < 1) {		// 예약번호(-1000을 한 두자리 배열숫자)가 배열번호를 초과했을 시
 				System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
-				continue;	// 다시 번호입력(상위)로 이동
+				continue;	// 다시 번호입력(상위)로 이동			
+			
+			} else {	// 알맞은 예약번호가 입력되었을 경우. 다음단계 진행.
 				
-			} else {
-				if (room[num-1] != 0) {						
-					System.out.println();
-					System.out.println("취소완료");
-					System.out.println();
-					room[num-1] = 0;
+				if (room[num-1] != 0) {		// 호실이 만실(예약이 되어있는 상태)인 경우 다음단계 진행.
+					
+					System.out.print("예약자:" + hostName[num-1] + "님 취소를 진행하시겠습니까? 예(1) / 아니오(2) : ");
+					int choice = scan.nextInt();	// 취소 진행여부를 물음
+					
+					if (choice == 1) {	// 진행을 할 경우.
+						System.out.println("※※※※취소완료※※※※");
+						System.out.println();
+						// 예약자의 모든 정보를 초기화
+						room[num-1] = 0;		// 방을 공실로 초기화
+						hostName[num-1] = "";	// 예약자명을 초기화
+						hostNum[num-1] = 0;		// 예약 인원수를 초기화
+						hostCode[num-1] = 0;	// 예약번호를 초기화
+						
+					} else if (choice == 2) {	// 취소진행을 하지 않을 경우
+						System.out.println("상위메뉴로 돌아갑니다.");
+						result = false;	// 해당 예약취소 메뉴를 나감.
+					}			
+					
 				} else {
 					System.out.println();
-					System.out.println("취소불가(이미 공실임)");
+					System.out.println("● 취소불가(예약번호가 잘못됬거나, 공실입니다)");
 					System.out.println();
+					
 				}
 				result = false;		// else문의 취소완료 및 불가목록 수행 후 while문 밖으로 이동(초기선택메뉴로)
 			}
-			
 			
 		}
 		
@@ -106,27 +116,37 @@ public class RoomTest02 {
 		
 	}
 
-	private static void RoomReserve(int[] room) {	// 예약진행 매서드
+	private static void roomReserve(int[] room, String[] hostName, int[] hostNum, int[] hostCode) {	// ●●●●● 예약진행 매서드
 		
 		boolean result = true;	// 이 boolean변수는 예약취소 매서드가 main밖에 위치해있기 때문에 함수 내에서 초기화를 해주어야함
 		
 		// 배열초과시 에러메세지 탈출코드
-		while (result) {	// 반복문 while을 입력해서 안에서 탈출 시 바로 아래 번호입력란으로 계속 돌아감
+		while (result) {	// 반복문 while을 입력해서 안에서 탈출 시 바로 아래 번호입력란으로 계속 돌아감						
 			System.out.print("예약객실 번호입력 : ");
-			int num = scan.nextInt();
+			int num = scan.nextInt();	// roomReserve() 밖에 num변수가 존재하기 때문에 맨 앞에 데이터타입을 입력해서 함수내의 num을 다시 초기화
 			
-			if (num > 12) {		// 예약취소번호가 배열번호를 초과했을 시
+			if (num > room.length || num < 1) {		// 예약번호(-1000을 한 두자리 배열숫자)가 배열번호를 초과했을 시
 				System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
 				continue;	// 다시 번호입력(상위)로 이동
-			} else {
-				if (room[num-1] != 1) {						
+				
+			} else {	// 알맞은 객실을 입력한 경우 다음단계로 진행.
+				if (room[num-1] != 1) {		// 객실이 공실일 경우 다음단계 진행.				
+					System.out.print("성함을 입력해주세요 : ");
+					hostName[num-1] = scan.next();	// 입력된 이름을 hostName 배열 해당 데이터에 저장
+					System.out.print("인원수를 입력하세요 : ");
+					hostNum[num-1] = scan.nextInt();	// 입력된 인원수를 hostNum 배열 해당 데이터에 저장
+					
+					hostCode[num-1] = 1000 + num;	// 예약하려는 호실넘버에 +1000을 하여 네자리의 예약번호 생성. 예) 12호실 = 1000 + 12 = 1012
+					
 					System.out.println();
-					System.out.println("예약완료");
+					System.out.println("※※※※예약완료※※※※");
+					System.out.println("[예약번호는 " + hostCode[num-1] + "입니다.]");
 					System.out.println();
-					room[num-1] = 1;
+					room[num-1] = 1;	// 예약된 방을 공실이 아닌 예약(1)로 변환
+					
 				} else {
 					System.out.println();
-					System.out.println("예약불가(이미 예약됨)");
+					System.out.println("● 예약불가(이미 예약됨)");
 					System.out.println();
 				}
 				result = false;	// else문의 취소완료 및 불가목록 수행 후 while문 밖으로 이동(초기선택메뉴로)
@@ -136,20 +156,21 @@ public class RoomTest02 {
 
 	}
 
-	private static void RoomList(int[] room) {	// 호실확인 매서드
+	private static void roomList(int[] room, String[] hostName, int[] hostNum, int[] hostCode) {	// ●●●●● 호실확인 매서드
 		System.out.println();
-		System.out.println("방 확인 -----------");
+		System.out.println("----- 방 확인 ---------------");
 		
 		for (int i=0; i<room.length; i++) {
-			System.out.print((i+1) + "번객실 : ");
+			System.out.print(" ▦ " + (i+1) + "번객실 : ");
+			
 			if (room[i] != 1) {
-				System.out.println("공실	|");
+				System.out.println("공실");
 			} else {
-				System.out.println("만실	|");
+				System.out.println(hostName[i] + " " + hostNum[i] + "명 [예약번호:" + hostCode[i] + "]");
 			}
 		}
 		
-		System.out.println("-----------------");
+		System.out.println("---------------------------");
 		System.out.println("");
 	}
 
